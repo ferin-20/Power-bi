b1f205446182b1b8d1afbfc84d60a853ee734cb8
ferin-20 <ferindamani@gmail.com>
2026-08-27 12:47:28 +0530

    Add files via upload

---
+++ SQLQuery 31.sql
@@ -0,0 +1,19 @@
+create database sales_transactions;
+
+
+create table sales_transactions(
+transaction_id int primary key ,
+customer_name varchar (50)
+product_name varchar (50)
+catagory varchar (50)
+quantity int
+unit_price int 
+discount_percentage int 
+city varchar(50)
+payment_mode varchar (30)
+salesperson varchar (50)
+customer_type (30)
+
+
+
+);
